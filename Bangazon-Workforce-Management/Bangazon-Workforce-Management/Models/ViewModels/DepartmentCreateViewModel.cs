914f6365b54e3f473695fc80c8096711164acfc3
Your Name <you@example.com>
2019-08-16 10:20:04 -0500

    fixed namespaces so they are the same across the project

--- Bangazon-Workforce-Management/Bangazon-Workforce-Management/Models/ViewModels/DepartmentCreateViewModel.cs
+++ Bangazon-Workforce-Management/Bangazon-Workforce-Management/Models/ViewModels/DepartmentCreateViewModel.cs
@@ -1,11 +1,73 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Bangazon-Workforce-Management.Models.ViewModels
+namespace Bangazon_Workforce_Management.Models.ViewModels
 {
     public class DepartmentCreateViewModel
 {
-}
+        public List<SelectListItem> Cohorts { get; set; }
+        public Department Department { get; set; }
+
+        private readonly string _connectionString;
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        //constructor
+        public StudentCreateViewModel() { }
+
+        public StudentCreateViewModel(string connectionString)
+        {
+            _connectionString = connectionString;
+
+            Cohorts = GetAllCohorts()
+                .Select(cohort => new SelectListItem
+                {
+                    Text = cohort.Name,
+                    Value = cohort.Id.ToString()
+                })
+                .ToList();
+            Cohorts.Insert(0, new SelectListItem
+            {
+                Text = "Choose cohort...",
+                Value = "0"
+            });
+        }
+
+        private List<Cohort> GetAllCohorts()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Cohort";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //below, use while for mult cohorts/whatever and if in place of while if you only want one thing
+                    List<Cohort> cohorts = new List<Cohort>();
+                    while (reader.Read())
+                    {
+                        cohorts.Add(new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return cohorts;
+                }
+            }
+        }
+    }
 }
