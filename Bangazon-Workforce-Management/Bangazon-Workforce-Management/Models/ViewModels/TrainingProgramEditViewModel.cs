27fae9e4fada0f07c4cbb0bcea0348317d349f81
Your Name <you@example.com>
2019-08-20 09:18:24 -0500

    hit a wall

--- Bangazon-Workforce-Management/Bangazon-Workforce-Management/Models/ViewModels/TrainingProgramEditViewModel.cs
+++ Bangazon-Workforce-Management/Bangazon-Workforce-Management/Models/ViewModels/TrainingProgramEditViewModel.cs
@@ -16,41 +16,41 @@ namespace Bangazon_Workforce_Management.Models.ViewModels
 
         public TrainingProgramEditViewModel(TrainingProgram trainingProgram) { }
 
-        public TrainingProgramEditViewModel(TrainingProgram trainingProgram, List<Employee> employeeList)
-        {
-            trainingProgram = trainingProgram;
-            Employees = employeeList
-                .Select(employee => new SelectListItem
-                {
-                    Text = employee.FullName,
-                    Value = employee.Id.ToString()
-                })
-                .ToList();
-            TrainingPrograms.Insert(0, new SelectListItem
-            {
-                Text = "Choose training program...",
-                Value = "0"
-            });
-        }
-
-
-
-        //public TrainingProgramEditViewModel((TrainingProgram trainingProgram, List<TrainingProgram> trainingProgramList)
+        //public TrainingProgramEditViewModel(TrainingProgram trainingProgram, List<Employee> employeeList)
         //{
-        //    TrainingProgram = trainingProgram;
-        //    TrainingPrograms = trainingProgramList
-        //        .Select(trainingProgram => new SelectListItem
+        //    trainingProgram = trainingProgram;
+        //    Employees = employeeList
+        //        .Select(employee => new SelectListItem
         //        {
-        //            Text = trainingProgram.Name,
-        //            Value = trainingProgram.Id.ToString()
+        //            Text = employee.FullName,
+        //            Value = employee.Id.ToString()
         //        })
         //        .ToList();
-
         //    TrainingPrograms.Insert(0, new SelectListItem
         //    {
         //        Text = "Choose training program...",
         //        Value = "0"
         //    });
         //}
+
+
+
+        public TrainingProgramEditViewModel((TrainingProgram trainingProgram, List<TrainingProgram> trainingProgramList)
+        {
+            TrainingProgram = trainingProgram;
+            TrainingPrograms = trainingProgramList
+                .Select(trainingProgram => new SelectListItem
+                {
+                    Text = trainingProgram.Name,
+                    Value = trainingProgram.Id.ToString()
+                })
+                .ToList();
+
+            TrainingPrograms.Insert(0, new SelectListItem
+            {
+                Text = "Choose training program...",
+                Value = "0"
+            });
+        }
     }
 }
